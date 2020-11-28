a461bab2ab55506910923053b0e88d063d8091e4
Raja CSP Raman <raja.csp@gmail.com>
2020-11-28 10:26:46 -0500

    Updates for 2020-11-28

--- app.py
+++ app.py
@@ -18,6 +18,50 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+'''
+    http://127.0.0.1:8000/
+'''
 @app.get("/")
 async def abc():
     return {"fav_food": "Biryani"}
+
+'''
+    http://127.0.0.1:8000/city?name=chennai
+'''
+@app.get("/city")
+def get_city(name: str):
+
+    _dict = {
+        "name" : name
+    }
+
+    return _dict
+
+
+'''
+    http://127.0.0.1:8000/city/chennai/
+'''
+@app.get("/city/{name}")
+def get_city_pathvariable(name: str):
+
+    _dict = {
+        "path_name" : name
+    }
+
+    return _dict
+
+# Post username
+
+
+'''
+    http://127.0.0.1:8000/city/xyz?name=hyd
+    POST
+'''
+@app.post("/city/xyz")
+def post_city_name(name: str):
+
+    _dict = {
+        "xyz_name" : name
+    }
+
+    return _dict
